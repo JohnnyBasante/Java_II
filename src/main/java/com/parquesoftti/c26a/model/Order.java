dbf1c167360cac28051fe5820c81a6da62cee57f
Johnny Basante <johnni1@hotmail.com>
2024-08-30 21:21:47 -0500

    primer commit

---
+++ src/main/java/com/parquesoftti/c26a/model/Order.java
@@ -0,0 +1,32 @@
+package com.parquesoftti.c26a.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "orders")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id",nullable = false)
+    private com.parquesoftti.c26a.model.Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id",nullable = false)
+    private Product product;
+
+    private LocalDateTime orderDate;
+    private Integer quantity;
+
+}
